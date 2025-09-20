3ad578b52a876fbbcbf6012aa06dcb25d12d15d7
wholesomeow <drcappuchino2@gmail.com>
2025-09-20 09:44:34 -0600

    restructured conversation/message structs to align with git principles better

--- cmd/app/toygit.go
+++ cmd/app/toygit.go
@@ -55,3 +55,6 @@ func (c *Conversation) Checkout(branchName string) error {
 
 	return fmt.Errorf("branch %s not found", branchName)
 }
+
+// TODO(wholesomeow): Make this function and then adapt it out
+// func (c *Conversation) Merge(branchA string, branchB string) error {}
